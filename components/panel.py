61f5aaf73adbe5b220c8a64ca83bfeb5f7591b7d
Juan Manuel Berros <juanmaberros@gmail.com>
2016-04-06 21:39:10 -0300

    Initial commit: Panel and Source

---
+++ components/panel.py
@@ -0,0 +1,131 @@
+import pandas as pd
+
+from os.path import join, isfile
+from components.source import Source
+from helpers.config import Config
+
+
+class Panel:
+    def __init__(self, panel_label):
+        """
+        - Reads an rs_id list from a <label>.bim file. Will also look for a
+        <label>.csv info file about the SNPs in the panel.
+        - To get genotypes from a given Source, put a <label>.traw of the panel
+        in the Source dir!
+        """
+        self.base_dir = Config('dirs')['panels']
+
+        bim_file = join(self.base_dir, panel_label + '.bim')
+        self.snps = self.read_bim(bim_file)
+
+        info_file = join(self.base_dir, panel_label + '.csv')
+        if isfile(info_file):
+            self.extra_info = self.read_info(info_file)
+
+        self.rs_ids = self.snps.index.values  # Redundant, but handy shortcut
+        self.label = panel_label
+        self.name = self._generate_name()
+        self.genotypes_cache = {}
+
+    def __repr__(self):
+        return '<Panel {}>'.format(self.name)
+
+    def genotypes(self, source_label, dataset=None):
+        if self.genotypes_cache.get(source_label) is None:
+            data = Source(source_label).genotypes(self.label)
+            self.genotypes_cache[source_label] = data
+
+        if dataset is None:
+            return self.genotypes_cache[source_label]
+
+        # Assmes a MultiIndex with levels: superpopulation, population, sample
+        slicer = pd.IndexSlice[:, :, dataset.sample_ids]
+        return self.genotypes_cache.loc[slicer, :]
+
+    #  def allele_freqs(self, level="population"):
+        #  genotypes = self.genotypes_1000G()
+        #  # allele_freqs = genotypes.groupby(level=level).sum()
+        #  total_obs = genotypes.count() * 2
+        #  return total_obs
+
+    def _generate_name(self):
+        # TODO: think about the Subpanel subclass
+        #  if "_from_" in self.label:  # Subpanel
+            #  name = self.label.replace("_", " ")
+        #  else:
+
+        #  is_AIMs_panel = ("GAL" in self.label)
+        #  if is_AIMs_panel:
+            #  name = name.replace("SNPs", "AIMs")
+
+        name = "{0} · {1:,} SNPs".format(self.label, len(self.rs_ids))
+        return name
+
+    #  def generate_subset_SNP_list(self, length, sort_key="LSBL(Fst)"):
+        #  """
+        #  This generates a .snps file with one rs_id per line.
+        #  The extraction of SNPs from the original .bed should be run in plink.
+        #  Afterwards, you can just read the new subpanel with Panel(label).
+        #  """
+        #  new_label = '{}_SNPs_from_{}'.format(length, self.label)
+        #  rs_ids_with_genotypes = self.genotypes_1000G().columns
+        #  snps_with_genotype = self.extra_info.loc[rs_ids_with_genotypes]
+        #  snps_with_genotype.sort_values(sort_key, ascending=False, inplace=True)
+        #  subpanel = snps_with_genotype.ix[:length, :]
+        #  filename = join(self.PANEL_INFO_DIR, new_label)
+        #  subpanel.to_csv(filename + ".csv",
+                        #  index_label=self.extra_info.index.name)
+        #  np.savetxt(filename + ".snps", subpanel.index.values, fmt="%s")
+
+        #  return filename
+
+    @staticmethod
+    def read_bim(filename):
+        bim_fields = ["chr", "rs_id", "morgans", "position", "A1", "A2"]
+        df = pd.read_table(filename, names=bim_fields, index_col="rs_id",
+                           usecols=["chr", "rs_id", "position", "A1", "A2"])
+        return df
+
+    @staticmethod
+    def read_info(filename):
+        return pd.read_csv(filename, index_col="rs_id")
+
+    #  @classmethod
+    #  def panel_groups(cls):
+        #  return {
+            #  "panels": cls.all_panels(),
+            #  "control_panels": cls.all_control_panels(),
+            #  "subpanels": cls.all_subpanels()
+        #  }
+
+    #  @classmethod
+    #  def all_panels_and_subpanels(cls):
+        #  glob_expr = join(cls.THOUSAND_GENOMES_DIR, "*.bim")
+        #  bim_files = [basename(path) for path in glob(glob_expr)]
+        #  labels = sorted([fn.replace(".bim", "") for fn in bim_files])
+
+        #  gal_panels = [label for label in labels if "GAL" in label]
+        #  control_panels = [label for label in labels if "CPx" in label]
+        #  subpanels = [label for label in labels if "_from_" in label and
+                     #  label not in gal_panels + control_panels]
+
+        #  return [cls(label) for label in gal_panels + control_panels + subpanels]
+
+    #  @classmethod
+    #  def all_panels(cls):
+        #  return [panel for panel in cls.all_panels_and_subpanels()
+                #  if "GAL" in panel.label and "_from_" not in panel.label]
+
+    #  @classmethod
+    #  def all_subpanels(cls, label=None):
+        #  all_panels = cls.all_panels_and_subpanels()
+        #  subpanels = [p for p in all_panels if "_from_" in p.label]
+        #  if label is not None:
+            #  subpanels = [sp for sp in subpanels if label in sp.label]
+        #  subpanels.sort(key=lambda p: len(p.rs_ids), reverse=True)
+        #  return subpanels
+
+    #  @classmethod
+    #  def all_control_panels(cls):
+        #  return [panel for panel in cls.all_panels_and_subpanels()
+                #  if "CPx" in panel.label and "_from_" not in panel.label]
---
+++ components/source.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import subprocess
+
+from os.path import join, isfile, basename
+from glob import glob
+from helpers.config import Config
+
+
+class Source:
+    def __init__(self, source_label):
+        """
+        Expects to find:
+            <base_dir>/<label>/samples.csv
+            # with fields: sample, population, superpopulation[, gender]
+
+            <base_dir>/<label>/populations.csv
+            # with fields: population, description, superpopulation
+
+            <base_dir>/label>/panels/<panel_label>.bed
+            (^ for any requested panel)
+            It will creat a .traw for panel if it doesn't find one.
+        """
+        self.base_dir = join(Config('dirs')['sources'], source_label)
+        self.panels_dir = join(self.base_dir, 'panels')
+        self.label = source_label
+        self.samples = self._read_samples()
+        self.populations = self._read_populations()
+
+    def __repr__(self):
+        return '<Source "{}">'.format(self.label)
+
+    def genotypes(self, panel_label):
+        filepath = join(self.panels_dir, panel_label + '.traw')
+        if not isfile(filepath):
+            self._create_traw_from_bed(panel_label)
+        return self._read_traw(filepath)
+
+    def has_panel(self, panel_label):
+        return panel_label in self.available_panels()
+
+    def available_panels(self):
+        bedfiles = glob(join(self.panels_dir, '*.bed'))
+        return [basename(bfile).replace('.bed', '') for bfile in bedfiles]
+
+    def _read_traw(self, filepath):
+        df = pd.read_table(filepath, index_col='SNP')
+        df.drop(['CHR', '(C)M', 'POS', 'COUNTED', 'ALT'], axis=1, inplace=True)
+        df.columns = [iid_fid.split('_')[1] for iid_fid in df.columns]
+        df.columns.name, df.index.name = 'sample', 'rs_id'
+        multi_index = ['superpopulation', 'population', 'sample']
+        df = self.samples.join(df.T).reset_index().set_index(multi_index)
+        return df.sort_index()
+
+    def _create_traw_from_bed(self, panel_label):
+        print('Calling plink to create a .traw for {}'.format(panel_label))
+        bedfile = join(self.panels_dir, panel_label)
+        if not isfile(bedfile + '.bed'):
+            raise OSError("Couldn't find {}.bed".format(bedfile))
+        subprocess.run([
+            'plink', '--bfile', bedfile,
+            '--recode', 'A-transpose',
+            '--out', bedfile
+        ])
+
+    def _read_samples(self):
+        samples = pd.read_csv(join(self.base_dir, 'samples.csv'))
+        if 'gender' in samples.columns:
+            samples.drop('gender', axis=1, inplace=True)
+        samples.set_index('sample', inplace=True)
+        return samples
+
+    def _read_populations(self):
+        populations = pd.read_csv(join(self.base_dir, 'populations.csv'))
+        populations.set_index('population', inplace=True)
+        return populations
+
+    #  def samples_from_pop_codes(self, pop_codes):
+        #  # Assumes pop_code as a column.
+        #  missing = setdiff1d(pop_codes, self.all_samples()["population"])
+        #  if len(missing) > 0:
+            #  raise ValueError("Couldn't find populations: {}".format(missing))
+
+        #  # Turn population into index temporarily to get the desired samples
+        #  # *in the order of the pop_codes*. Then set the original index back.
+        #  filtered = self.all_samples().reset_index().set_index("population").loc[pop_codes]
+        #  return filtered.reset_index().set_index("sample").dropna()
+
+    #  @classmethod
+    #  def population_names(cls):
+        #  if isfile(cls.POP_NAMES_FILE):
+            #  return pd.read_csv(cls.POP_NAMES_FILE, index_col="population")
+
+        #  df = cls._get_pop_names_from_url()
+        #  keep_these_columns = ["Population Code", "Population Description",
+                              #  "Super Population Code"]
+        #  df = df[keep_these_columns]
+        #  df.columns = ["population", "description", "superpopulation"]
+        #  df.set_index("population", inplace=True)
+
+        #  df.to_csv(cls.POP_NAMES_FILE)
+        #  return df
+
